be5c15f0a01afc343064c9f8e5678abe3522a450
Kwame <68175563+kamosah@users.noreply.github.com>
2026-03-22 20:51:00 -0400

    content: add custom select dropdown challenge to custom-form-controls topic

--- src/challenges/custom-form-controls/index.ts
+++ src/challenges/custom-form-controls/index.ts
@@ -3,9 +3,10 @@ import { challenge as customRadioButton } from './custom-radio-button/meta'
 import { challenge as customFileUpload } from './custom-file-upload/meta'
 import { challenge as customToggleSwitch } from './custom-toggle-switch/meta'
 import { challenge as customRangeSlider } from './custom-range-slider/meta'
+import { challenge as customSelectDropdown } from './custom-select-dropdown/meta'
 
 export const topic: Topic = {
   id: 'custom-form-controls',
   title: 'Custom Form Controls',
-  challenges: [customRadioButton, customFileUpload, customToggleSwitch, customRangeSlider],
+  challenges: [customRadioButton, customFileUpload, customToggleSwitch, customRangeSlider, customSelectDropdown],
 }
