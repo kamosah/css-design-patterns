b0207df0d866eac3fa193f9fe77f1692c19f30ff
kamosah <68175563+kamosah@users.noreply.github.com>
2026-04-01 08:43:05 -0400

    feat: add responsive testimonial carousel and grid visibility challenge to responsive-visibility topic

--- src/challenges/responsive-visibility/index.ts
+++ src/challenges/responsive-visibility/index.ts
@@ -5,9 +5,10 @@ import { challenge as blogSidebarVisibility } from './blog-sidebar-visibility/me
 import { challenge as mobileContactButtonVisibility } from './mobile-contact-button-visibility/meta'
 import { challenge as footerSocialIconsVisibility } from './footer-social-icons-visibility/meta'
 import { challenge as responsiveArticleGridSidebarToggle } from './responsive-article-grid-sidebar-toggle/meta'
+import { challenge as responsiveTestimonialCarouselGridVisibility } from './responsive-testimonial-carousel-grid-visibility/meta'
 
 export const topic: Topic = {
   id: 'responsive-visibility',
   title: 'Responsive Visibility',
-  challenges: [mobileNavToggle, promotionalBannerVisibility, blogSidebarVisibility, mobileContactButtonVisibility, footerSocialIconsVisibility, responsiveArticleGridSidebarToggle],
+  challenges: [mobileNavToggle, promotionalBannerVisibility, blogSidebarVisibility, mobileContactButtonVisibility, footerSocialIconsVisibility, responsiveArticleGridSidebarToggle, responsiveTestimonialCarouselGridVisibility],
 }
