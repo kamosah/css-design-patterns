551d8ccbb855d0632dcca0f7cb196d0665d08ffb
Kwame <68175563+kamosah@users.noreply.github.com>
2026-04-07 15:14:35 -0400

    feat: add inline-block group centering challenge
    Adds an easy challenge teaching text-align: center as the mechanism
    for centering inline-block children within a fixed-width container.

--- src/challenges/centering-alignment/index.ts
+++ src/challenges/centering-alignment/index.ts
@@ -1,10 +1,11 @@
 import type { Topic } from '../../types/challenge'
 import { challenge as horizontalCentering } from './horizontal-centering-margin-auto/meta'
+import { challenge as inlineBlockGroupCentering } from './inline-block-group-centering/meta'
 import { challenge as verticalCentering } from './vertical-centering-flexbox/meta'
 import { challenge as fullCentering } from './full-centering-flexbox/meta'
 
 export const topic: Topic = {
   id: 'centering-alignment',
   title: 'Centering & Alignment',
-  challenges: [horizontalCentering, verticalCentering, fullCentering],
+  challenges: [horizontalCentering, inlineBlockGroupCentering, verticalCentering, fullCentering],
 }
