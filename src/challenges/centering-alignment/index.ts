2a49813aacd072afa1eae902724b8bc0dd4f17c7
kamosah <68175563+kamosah@users.noreply.github.com>
2026-04-08 17:40:39 -0400

    feat: add css modal centering fallbacks challenge
    Adds a centering challenge focused on progressively-enhanced modal centering: absolute+transform fallback with a modern `inset + margin: auto` override.

--- src/challenges/centering-alignment/index.ts
+++ src/challenges/centering-alignment/index.ts
@@ -9,9 +9,10 @@ import { challenge as mediaObjectAlignment } from './media-object-alignment/meta
 import { challenge as toolbarSpaceBetween } from './toolbar-space-between/meta'
 import { challenge as flexboxCenteringMixedHeight } from './flexbox-centering-mixed-height/meta'
 import { challenge as absolutePositionCenteringPadded } from './absolute-position-centering-padded/meta'
+import { challenge as cssModalCenteringFallbacks } from './css-modal-centering-fallbacks/meta'
 
 export const topic: Topic = {
   id: 'centering-alignment',
   title: 'Centering & Alignment',
-  challenges: [horizontalCentering, inlineBlockGroupCentering, verticalCentering, fullCentering, fullCenteringGrid, baselineAlignmentImageText, mediaObjectAlignment, toolbarSpaceBetween, flexboxCenteringMixedHeight, absolutePositionCenteringPadded],
+  challenges: [horizontalCentering, inlineBlockGroupCentering, verticalCentering, fullCentering, fullCenteringGrid, baselineAlignmentImageText, mediaObjectAlignment, toolbarSpaceBetween, flexboxCenteringMixedHeight, absolutePositionCenteringPadded, cssModalCenteringFallbacks],
 }
