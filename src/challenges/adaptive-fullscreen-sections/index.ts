d2d39299c3023a9b0ce23357d722f343680b590b
kamosah <68175563+kamosah@users.noreply.github.com>
2026-04-11 17:33:10 -0400

    feat: add fullscreen section with image background challenge

--- src/challenges/adaptive-fullscreen-sections/index.ts
+++ src/challenges/adaptive-fullscreen-sections/index.ts
@@ -1,9 +1,10 @@
 import type { Topic } from '../../types/challenge'
 import { challenge as simpleAdaptiveFullscreenHero } from './simple-adaptive-fullscreen-hero/meta'
 import { challenge as fullscreenSectionBackgroundColor } from './fullscreen-section-background-color/meta'
+import { challenge as fullscreenSectionImageBackground } from './fullscreen-section-image-background/meta'
 
 export const topic: Topic = {
   id: 'adaptive-fullscreen-sections',
   title: 'Adaptive Fullscreen Sections',
-  challenges: [simpleAdaptiveFullscreenHero, fullscreenSectionBackgroundColor],
+  challenges: [simpleAdaptiveFullscreenHero, fullscreenSectionBackgroundColor, fullscreenSectionImageBackground],
 }
