950c62006787f43b2a3435003051aaa41243474e
kamosah <68175563+kamosah@users.noreply.github.com>
2026-04-11 09:50:29 -0400

    feat: add adaptive fullscreen sections topic with hero challenge

---
+++ src/challenges/adaptive-fullscreen-sections/index.ts
@@ -0,0 +1,8 @@
+import type { Topic } from '../../types/challenge'
+import { challenge as simpleAdaptiveFullscreenHero } from './simple-adaptive-fullscreen-hero/meta'
+
+export const topic: Topic = {
+  id: 'adaptive-fullscreen-sections',
+  title: 'Adaptive Fullscreen Sections',
+  challenges: [simpleAdaptiveFullscreenHero],
+}
