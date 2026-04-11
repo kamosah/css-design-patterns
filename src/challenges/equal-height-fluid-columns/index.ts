719d910f23940d226f22d1dd09a70e0aae5734aa
kamosah <68175563+kamosah@users.noreply.github.com>
2026-04-11 17:21:41 -0400

    feat: add columns fluid layout challenge

--- src/challenges/equal-height-fluid-columns/index.ts
+++ src/challenges/equal-height-fluid-columns/index.ts
@@ -2,9 +2,10 @@ import type { Topic } from '../../types/challenge'
 import { challenge as flexEqualHeightColumns } from './flex-equal-height-columns/meta'
 import { challenge as tableDisplayEqualHeight } from './table-display-equal-height/meta'
 import { challenge as gridAlignItemsStretch } from './grid-align-items-stretch/meta'
+import { challenge as columnsFluidLayout } from './columns-fluid-layout/meta'
 
 export const topic: Topic = {
   id: 'equal-height-fluid-columns',
   title: 'Equal Height & Fluid Columns',
-  challenges: [flexEqualHeightColumns, tableDisplayEqualHeight, gridAlignItemsStretch],
+  challenges: [flexEqualHeightColumns, tableDisplayEqualHeight, gridAlignItemsStretch, columnsFluidLayout],
 }
