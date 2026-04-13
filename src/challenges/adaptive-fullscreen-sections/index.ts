dd8ebc27f6700de460172c83ab5338fd4102d818
Kwame <68175563+kamosah@users.noreply.github.com>
2026-04-12 23:19:07 -0400

    feat: add fullscreen section centered logo challenge

--- src/challenges/adaptive-fullscreen-sections/index.ts
+++ src/challenges/adaptive-fullscreen-sections/index.ts
@@ -5,9 +5,10 @@ import { challenge as fullscreenSectionImageBackground } from './fullscreen-sect
 import { challenge as adaptiveFullscreenMobileSafeHeight } from './adaptive-fullscreen-mobile-safe-height/meta'
 import { challenge as fullscreenSectionResponsivePadding } from './fullscreen-section-responsive-padding/meta'
 import { challenge as fullscreenSectionScrollIndicator } from './fullscreen-section-scroll-indicator/meta'
+import { challenge as fullscreenSectionCenteredLogo } from './fullscreen-section-centered-logo/meta'
 
 export const topic: Topic = {
   id: 'adaptive-fullscreen-sections',
   title: 'Adaptive Fullscreen Sections',
-  challenges: [simpleAdaptiveFullscreenHero, fullscreenSectionBackgroundColor, fullscreenSectionImageBackground, adaptiveFullscreenMobileSafeHeight, fullscreenSectionResponsivePadding, fullscreenSectionScrollIndicator],
+  challenges: [simpleAdaptiveFullscreenHero, fullscreenSectionBackgroundColor, fullscreenSectionImageBackground, adaptiveFullscreenMobileSafeHeight, fullscreenSectionResponsivePadding, fullscreenSectionScrollIndicator, fullscreenSectionCenteredLogo],
 }
