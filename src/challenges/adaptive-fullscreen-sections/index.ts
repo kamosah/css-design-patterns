4f60ddcf6070d87e0a4a90a8fe2b6c3afdbf267a
kamosah <68175563+kamosah@users.noreply.github.com>
2026-04-13 06:24:35 -0400

    feat: add fullscreen section clamped subheading challenge

--- src/challenges/adaptive-fullscreen-sections/index.ts
+++ src/challenges/adaptive-fullscreen-sections/index.ts
@@ -8,9 +8,10 @@ import { challenge as fullscreenSectionScrollIndicator } from './fullscreen-sect
 import { challenge as fullscreenSectionCenteredLogo } from './fullscreen-section-centered-logo/meta'
 import { challenge as fullscreenSectionMaxWidthContent } from './fullscreen-section-max-width-content/meta'
 import { challenge as fullscreenSectionGradientOverlay } from './fullscreen-section-gradient-overlay/meta'
+import { challenge as fullscreenSectionClampedSubheading } from './fullscreen-section-clamped-subheading/meta'
 
 export const topic: Topic = {
   id: 'adaptive-fullscreen-sections',
   title: 'Adaptive Fullscreen Sections',
-  challenges: [simpleAdaptiveFullscreenHero, fullscreenSectionBackgroundColor, fullscreenSectionImageBackground, adaptiveFullscreenMobileSafeHeight, fullscreenSectionResponsivePadding, fullscreenSectionScrollIndicator, fullscreenSectionCenteredLogo, fullscreenSectionMaxWidthContent, fullscreenSectionGradientOverlay],
+  challenges: [simpleAdaptiveFullscreenHero, fullscreenSectionBackgroundColor, fullscreenSectionImageBackground, adaptiveFullscreenMobileSafeHeight, fullscreenSectionResponsivePadding, fullscreenSectionScrollIndicator, fullscreenSectionCenteredLogo, fullscreenSectionMaxWidthContent, fullscreenSectionGradientOverlay, fullscreenSectionClampedSubheading],
 }
