ccb7e20849521bd4e9bb6dc91ed2545913f016fe
Kwame <68175563+kamosah@users.noreply.github.com>
2026-04-14 02:40:44 -0400

    feat: add custom-property controlled column layout challenge

--- src/challenges/equal-height-fluid-columns/index.ts
+++ src/challenges/equal-height-fluid-columns/index.ts
@@ -8,9 +8,10 @@ import { challenge as fluidGridColumnsAutoFit } from './fluid-grid-columns-auto-
 import { challenge as containerQueryFluidColumns } from './container-query-fluid-columns/meta'
 import { challenge as responsiveFlexboxVariableColumns } from './responsive-flexbox-variable-columns/meta'
 import { challenge as balancedMultiColumnContent } from './balanced-multi-column-content/meta'
+import { challenge as customPropertyControlledColumnLayout } from './custom-property-controlled-column-layout/meta'
 
 export const topic: Topic = {
   id: 'equal-height-fluid-columns',
   title: 'Equal Height & Fluid Columns',
-  challenges: [flexEqualHeightColumns, tableDisplayEqualHeight, gridAlignItemsStretch, columnsFluidLayout, flexWrapFluidColumns, fluidGridColumnsAutoFit, containerQueryFluidColumns, responsiveFlexboxVariableColumns, balancedMultiColumnContent],
+  challenges: [flexEqualHeightColumns, tableDisplayEqualHeight, gridAlignItemsStretch, columnsFluidLayout, flexWrapFluidColumns, fluidGridColumnsAutoFit, containerQueryFluidColumns, responsiveFlexboxVariableColumns, balancedMultiColumnContent, customPropertyControlledColumnLayout],
 }
