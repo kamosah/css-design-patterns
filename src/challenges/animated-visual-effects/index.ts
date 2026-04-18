58be454b82a657f66f434b544ff009c398aafe8e
Kwame <68175563+kamosah@users.noreply.github.com>
2026-04-18 18:39:03 -0400

    feat: add rotating loading spinner challenge to animated visual effects topic

--- src/challenges/animated-visual-effects/index.ts
+++ src/challenges/animated-visual-effects/index.ts
@@ -1,9 +1,10 @@
 import type { Topic } from '../../types/challenge'
 import { challenge as hoverZoomImage } from './hover-zoom-image/meta'
 import { challenge as fadeInNotificationBanner } from './fade-in-notification-banner/meta'
+import { challenge as rotatingLoadingSpinner } from './rotating-loading-spinner/meta'
 
 export const topic: Topic = {
   id: 'animated-visual-effects',
   title: 'Animated Visual Effects',
-  challenges: [hoverZoomImage, fadeInNotificationBanner],
+  challenges: [hoverZoomImage, fadeInNotificationBanner, rotatingLoadingSpinner],
 }
