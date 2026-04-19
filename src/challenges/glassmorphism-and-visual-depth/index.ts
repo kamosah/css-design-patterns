21afa6424c69dd3c0c38b06ae8621f3a4595f028
kamosah <68175563+kamosah@users.noreply.github.com>
2026-04-19 17:59:52 -0400

    feat: add glass header navbar challenge to glassmorphism topic

--- src/challenges/glassmorphism-and-visual-depth/index.ts
+++ src/challenges/glassmorphism-and-visual-depth/index.ts
@@ -1,8 +1,9 @@
 import type { Topic } from '../../types/challenge'
 import { challenge as glassCardComponent } from './glass-card-component/meta'
+import { challenge as glassHeaderNavbar } from './glass-header-navbar/meta'
 
 export const topic: Topic = {
   id: 'glassmorphism-and-visual-depth',
   title: 'Glassmorphism and Visual Depth',
-  challenges: [glassCardComponent],
+  challenges: [glassCardComponent, glassHeaderNavbar],
 }
