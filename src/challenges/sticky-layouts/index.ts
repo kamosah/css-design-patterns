2a879ae96b38bb80a4b582c2b72a23b04eb0048e
Kwame <68175563+kamosah@users.noreply.github.com>
2026-04-19 09:43:59 -0400

    feat: add overlapping sticky banner challenge to sticky layouts topic

--- src/challenges/sticky-layouts/index.ts
+++ src/challenges/sticky-layouts/index.ts
@@ -5,9 +5,10 @@ import { challenge as persistentStickyFooter } from './persistent-sticky-footer/
 import { challenge as stickyElementWithOffset } from './sticky-element-with-offset/meta'
 import { challenge as stickyTableHeader } from './sticky-table-header/meta'
 import { challenge as scrollableContainerSectionStickiness } from './scrollable-container-section-stickiness/meta'
+import { challenge as overlappingStickyBanner } from './overlapping-sticky-banner/meta'
 
 export const topic: Topic = {
   id: 'sticky-layouts',
   title: 'Sticky Layouts',
-  challenges: [basicStickyHeader, stickySidebarNavigation, persistentStickyFooter, stickyElementWithOffset, stickyTableHeader, scrollableContainerSectionStickiness],
+  challenges: [basicStickyHeader, stickySidebarNavigation, persistentStickyFooter, stickyElementWithOffset, stickyTableHeader, scrollableContainerSectionStickiness, overlappingStickyBanner],
 }
