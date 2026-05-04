55f4a2ac3354c3215055d1c2256b54191a397b6a
kamosah <68175563+kamosah@users.noreply.github.com>
2026-05-04 08:07:54 -0400

    feat: add fluid button width challenge

--- src/challenges/intrinsic-and-container-based-sizing/index.ts
+++ src/challenges/intrinsic-and-container-based-sizing/index.ts
@@ -1,4 +1,5 @@
 import type { Topic } from '../../types/challenge'
+import { challenge as fluidButtonWidth } from './fluid-button-width/meta'
 import { challenge as fluidImageResizer } from './fluid-image-resizer/meta'
 import { challenge as responsiveVideoEmbed } from './responsive-video-embed/meta'
 import { challenge as squareBoxFiller } from './square-box-filler/meta'
@@ -6,5 +7,5 @@ import { challenge as squareBoxFiller } from './square-box-filler/meta'
 export const topic: Topic = {
   id: 'intrinsic-and-container-based-sizing',
   title: 'Intrinsic and Container Based Sizing',
-  challenges: [fluidImageResizer, squareBoxFiller, responsiveVideoEmbed],
+  challenges: [fluidImageResizer, squareBoxFiller, responsiveVideoEmbed, fluidButtonWidth],
 }
