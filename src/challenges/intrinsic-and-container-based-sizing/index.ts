8ba1d0e565c0a7db9b23354411ae38683a0dcb6a
kamosah <68175563+kamosah@users.noreply.github.com>
2026-05-11 08:22:37 -0400

    revert: remove adaptive testimonial grid challenge

--- src/challenges/intrinsic-and-container-based-sizing/index.ts
+++ src/challenges/intrinsic-and-container-based-sizing/index.ts
@@ -1,7 +1,6 @@
 import type { Topic } from '../../types/challenge'
 import { challenge as adaptiveCard } from './adaptive-card/meta'
 import { challenge as adaptiveProfileCard } from './adaptive-profile-card/meta'
-import { challenge as adaptiveTestimonialGrid } from './adaptive-testimonial-grid/meta'
 import { challenge as dashboardWidgetLayout } from './dashboard-widget-layout/meta'
 import { challenge as featureGridContainerQueries } from './feature-grid-container-queries/meta'
 import { challenge as fluidButtonWidth } from './fluid-button-width/meta'
@@ -25,6 +24,5 @@ export const topic: Topic = {
     featureGridContainerQueries,
     adaptiveProfileCard,
     dashboardWidgetLayout,
-    adaptiveTestimonialGrid,
   ],
 }
