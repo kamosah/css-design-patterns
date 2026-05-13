045f6eea94bb4ea6397c8d52515a3d33953179cf
kamosah <68175563+kamosah@users.noreply.github.com>
2026-05-13 09:20:32 -0400

    feat: add single-line text truncation and multi-line text clamping challenges

--- src/challenges/text-truncation-and-clamping/index.ts
+++ src/challenges/text-truncation-and-clamping/index.ts
@@ -1,8 +1,9 @@
 import type { Topic } from '../../types/challenge'
 import { challenge as singleLineTextTruncation } from './single-line-text-truncation/meta'
+import { challenge as multiLineTextClamping } from './multi-line-text-clamping/meta'
 
 export const topic: Topic = {
   id: 'text-truncation-and-clamping',
   title: 'Text Truncation and Clamping',
-  challenges: [singleLineTextTruncation],
+  challenges: [singleLineTextTruncation, multiLineTextClamping],
 }
