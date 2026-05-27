b9598369bff4f0eb513a0205cc745213e501f4cc
kamosah <68175563+kamosah@users.noreply.github.com>
2026-05-26 21:57:04 -0400

    feat: add Fluid Hero Heading and Subheading challenge to fluid-and-responsive-typography topic

--- src/challenges/fluid-and-responsive-typography/index.ts
+++ src/challenges/fluid-and-responsive-typography/index.ts
@@ -5,9 +5,10 @@ import { challenge as fluidButtonLabel } from './fluid-button-label/meta'
 import { challenge as fluidCaptionText } from './fluid-caption-text/meta'
 import { challenge as fluidNavigationLinkText } from './fluid-navigation-link-text/meta'
 import { challenge as fluidModularScale } from './fluid-modular-scale/meta'
+import { challenge as fluidHeroHeadingAndSubheading } from './fluid-hero-heading-and-subheading/meta'
 
 export const topic: Topic = {
   id: 'fluid-and-responsive-typography',
   title: 'Fluid and Responsive Typography',
-  challenges: [fluidHeadingResize, fluidParagraphText, fluidButtonLabel, fluidCaptionText, fluidNavigationLinkText, fluidModularScale],
+  challenges: [fluidHeadingResize, fluidParagraphText, fluidButtonLabel, fluidCaptionText, fluidNavigationLinkText, fluidModularScale, fluidHeroHeadingAndSubheading],
 }
